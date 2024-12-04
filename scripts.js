19c5f79a3615fce0ee976c7993c90f5b85bf13d0
Sumit Srivastava <srisumit4@gmail.com>
2024-12-04 15:43:43 +0530

    testing landing page

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Smooth Scroll to Section
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+// Dummy Ledger Function
+function viewLedger() {
+    alert("Redirecting to the expense ledger...");
+}
